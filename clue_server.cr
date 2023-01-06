75ec0bc3f9ad1af66c835bd64f0cfab612ea41fd
JayTeeSF <jaytee@jayteesf.com>
2023-01-05 16:38:26 -0800

    update server to serve other files

--- clue_server.cr
+++ clue_server.cr
@@ -1,3 +1,5 @@
+# https://btihen.me/explore/crystal/crystal_web_ssl_server.html
+# require "openssl"
 require "http/server"
 
 # crystal build -Dpreview_mt --release clue_server.cr
@@ -46,42 +48,39 @@ port = ((ARGV[1]? ? ARGV[1] : "9292").to_i)
 # server = HTTP::Server.new([HTTP::CompressHandler.new, HTTP::ErrorHandler.new, HTTP::LogHandler.new]) do |context|
 #
 
+# server = HTTP::Server.new([HTTP::CompressHandler.new, HTTP::ErrorHandler.new, HTTP::StaticFileHandler.new("./public")]) do |context|
 server = HTTP::Server.new([HTTP::CompressHandler.new, HTTP::ErrorHandler.new]) do |context|
   st = Time.utc
   req = context.request
   res = context.response
   if req.method == "GET"
-    file = nil
-    case req.resource
-    when "/clue", "/"
-      file = "index.html"
+    file = req.path
+    file = file.starts_with?("/") ? file[1..-1] : file
+    # 0. req.path: "/setup.html"
+    # 1.2. file_path: "./public/setup.html"
+    # 1.2a. ...
+    # 2. file_path: "/setup.html"
+    # puts "0. req.path: #{file.inspect}"
+    if file == "clue" || file == ""
       res.content_type = HTMLMime
-    when "/clue/bootstrap.min.css"
-      file = "bootstrap.min.css"
-      res.content_type = CSSMime
-    when "/clue/script.css"
-      file = "script.css"
-      res.content_type = CSSMime
-    when "/clue/jquery-3.6.3.slim.min.js"
-      file = "jquery-3.6.3.slim.min.js"
-      res.content_type = JSMime
-    when "/clue/script.js"
-      file = "script.js"
-      res.content_type = JSMime
-    when "/clue/clue.js"
-      file = "clue.js"
-      res.content_type = JSMime
-    when "/clue/bootstrap.bundle.min.js"
-      file = "bootstrap.bundle.min.js"
-      res.content_type = JSMime
-    when "/clue/favicon.ico", "/favicon.ico"
-      file = nil
+      file_path = "./public/index.html"
+      # puts "1.1 file_path: #{file_path.inspect}"
     else
-      file = nil
-    end
-    
-    if file
+      file = file.starts_with?("clue/") ? file[5..-1] : file
       file_path = "./public/#{file}"
+      # puts "1.2. file_path: #{file_path.inspect}"
+      if file && File.exists?(file_path)
+        # puts "1.2a. ..."
+        _, res.content_type = ext_and_mime_type_for(file_path)
+      else
+        # puts "1.2b. file: nil..."
+        file = nil
+      end
+    end
+
+    if file
+      # file_path = file #"./public/#{file}"
+      # puts "2. file_path: #{file_path.inspect}"
       content = File.read(file_path)
       res.output << content
       res.flush
@@ -92,6 +91,16 @@ server = HTTP::Server.new([HTTP::CompressHandler.new, HTTP::ErrorHandler.new]) d
 
   next
 end
+
+## configure the ssl service
+#tls_config = OpenSSL::SSL::Context::Server.new
+#tls_config.private_key = "./server.key"
+#tls_config.certificate_chain = "./server.crt"
+#
+## 'bind' the ssl server to the web server
+#server.bind_tls ip, port, tls_config
+#puts "Listening on https://#{ip}:#{port}"
+#server.listen
 
 puts "Listening on http://#{ip}:#{port}"
 server.listen(ip, port)
